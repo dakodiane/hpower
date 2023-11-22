bbc4c91ab42f6660c8de40f8b8ba297fefb7c6af
dakodiane <dianedako915@gmail.com>
2023-11-22 18:19:59 +0100

    444

--- hpower/app/Models/paiement.php
+++ hpower/app/Models/paiement.php
@@ -8,37 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Paiement extends Model
 {
     use HasFactory;
-    protected $table = 'paiements'; 
+    protected $table = 'paiement'; 
     protected $primaryKey = 'paie_id';
 
     protected $fillable = [
-        'prix_unit',
-        'qte_totale',
-         'prix_total',
-        'cam_id',
-        'date_paiement',
-        'created_at',
-        'updated_at',
-        'prix_tp',
-        'prix_HPG',
-        'montant_tp',
-        'montant_HPG',
-        'recette_HPG',
-        'solde',
-       'entrep_benef',
-       'net_paye',
-       'rest_paie',
-        'paietotal',
-        'statut_paie',
-        'date_paie',
         "paie_prixlivraison",
+        "prix_tp",
+        "prix_HPG",
+        "montant_tp",
+        "montant_HPG",
         "prod_magasin",
         "prod_lieuprod",
-
     ];
-
-    public function utilisateur()
-    {
-        return $this->belongsTo(User::class, 'util_id');
-    }
 }
