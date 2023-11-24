57657113f0a0f293e34e1eaa9bd844a8b326b828
dakodiane <dianedako915@gmail.com>
2023-11-24 12:25:28 +0100

    rzuhé

--- hpower/resources/views/Admin/servicesemence.blade.php
+++ hpower/resources/views/Admin/servicesemence.blade.php
@@ -3,46 +3,66 @@
 @section('document')
 <div class="main-panel">
     <div class="content-wrapper">
-        <div class="row">
+    <div class="row">
             <div class="col-lg-12 grid-margin stretch-card">
                 <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Semences Vendues </h4>
+                    <a href="{{ route('export.excel') }}" class="btn btn-success">Télécharger Excel</a>
+
+                    <div class="card-body" style="overflow-x: scroll;">
+                        <h4 class="card-title">Réception des semences </h4>
 
                         <div class="table-responsive">
+                            <h2>Réceptions Enregistrées</h2>
+
                             @if(!is_null($semences) && count($semences) > 0)
-                            <table class="tableau">
+                            <table class="table table-striped table-bordered table-sm">
                                 <thead>
                                     <tr>
-                                        <th>Date de Paie</th>
-                                        <th>Prix Unitaire HPG</th>
-                                        <th>Quantité Vendue</th>
-                                        <th>Client</th>
-                                        <th>Lieu de semi</th>
-                                        <th>MontantHPG</th>
-                                        <th>Recette</th>
+                                        <th>N° Bordereau</th>
+                                        <th>Semence</th>
+                                        <th>Quantité livrée</th>
+                                        <th>Fournisseur</th>
+                                        <th>Magasin de déchargement</th>
+                                        <th>Nature de la semence</th>
+                                        <th>Lieu de production</th>
+                                        <th>Prix unitaire</th>
+                                        <th>Prix de livraison</th>
+                                        <th>Date du paiement</th>
+                                        <th>Transaction Number</th>
+                                        <th>Immatriculation</th>
+                                        <th>Moyen de deplacement</th>
+                                        <th>Action</th>
                                     </tr>
-                                </thead> 
-                                @foreach ($semences as $semence)
-                                <tbody>                              
+                                </thead>
+                                <tbody>
+                                    @foreach($semences as $semence)
                                     <tr>
+                                        <td>{{ $semence->sem_bord }}</td>
+                                        <td>{{ $semence->sem_nature }}</td>
+                                        <td>{{ $semence->sem_qtereçu }}</td>
+                                        <td>{{ $semence->sem_fourni }}</td>
+                                        <td>{{ $semence->sem_magdecht }}</td>
+                                        <td>{{ $semence->sem_type }}</td>
+                                        <td>{{ $semence->sem_prove }}</td>
+                                        <td>{{ $semence->sem_prixunit }}</td>
+                                        <td>{{ $semence->sem_prixunit * $semence->sem_qtereçu }}</td>
                                         <td>{{now()->format('d-m-Y')}}</td>
-                                        <td>{{ $semence->sem_prixunitHPG }}</td>
-                                        <td>{{ $semence->sem_qtevendue }}</td>
-                                        <td>{{ $semence->sem_client }}</td>
-                                        <td>{{ $semence->sem_lieusemi }}</td>
-                                        <td>{{ $semence->sem_prixunitHPG * $semence->sem_qtevendu }}</td>
-                                        <td>{{ $semence->montantHPG - $semence->paie_prixlivraison }}</td>
+                                        <td>{{ $semence->sem_numtrans }}</td>
+                                        <td><a href="{{ asset($semence->semence_numtrans) }}" type="button" class="btn btn-success btn-md">Voir la photo</a></td>
+                                        <td>{{ $semence->sem_deplace }}</td>
+                               
                                     </tr>
-       
-                                </tbody> 
-                                @endforeach
+                                    @endforeach
+                                </tbody>
                             </table>
-                            @else
-                                <p>Aucune réception enregistrée.</p> 
-                            @endif
                             <!-- <a href="{{ route('exportExcel', ['viewType' => 'servconsultation']) }}" class="btn btn-success">Télécharger Excel</a> -->
-                            <br><br><div style="width:500px; height: 25px">{{ $semences->links() }}</div>
+
+                            @else
+                            <p>Aucune réception enregistrée.</p>
+                            @endif
+
+                            <br><br>
+                            <div style="width:500px; height: 25px">{{ $semences->links() }}</div>
                         </div>
                     </div>
                 </div>
